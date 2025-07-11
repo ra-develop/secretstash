c0424415cd01937eafdb1b8c892f11a5bf8b11bb
Rashid Amanzholov <rashid.amanzholov@gmail.com>
2025-07-11 19:41:08 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/secretstash/SecretStashApplication.kt
@@ -0,0 +1,11 @@
+package com.example.secretstash
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecretStashApplication
+
+fun main(args: Array<String>) {
+	runApplication<SecretStashApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/secretstash/SecretStashApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.secretstash
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecretStashApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
